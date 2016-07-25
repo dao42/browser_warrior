3d0740a7823fe97cf342f9829ebe0920c9f0caf9
yafeilee <lyfi2003@gmail.com>
2016-07-25 11:40:26 +0800

    Adjust ie6 display

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -10,6 +10,7 @@ module Dummy
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # config.i18n.default_locale = 'zh-CN'
   end
 end
 
